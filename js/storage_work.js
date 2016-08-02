dfec5eaa507eeaae5ecfd933de0b49654eb6d111
KindSamarit <kindsamarit@gmail.com>
2016-08-02 17:22:03 +0300

    fat-311 debug json

--- js/storage_work.js
+++ js/storage_work.js
@@ -2,8 +2,12 @@
 (function() {
     var Driver = {
         loadItem: function () {
-            var arrStorage = JSON.parse(localStorage.getItem("to-do"));
-            return arrStorage;
+            if (arrStorage) {
+                var arrStorage = JSON.parse(localStorage.getItem("to-do"));
+                return arrStorage;
+            } else {
+                return false;
+            }
         },
         saveItems: function (posts) {
             localStorage.setItem("to-do", JSON.stringify(posts));
